a5b8721eedc660ba7265365a55d2c694e6c799a3
Dio Eracler <dioeracler1988@gmail.com>
2024-08-28 12:33:39 +0300

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/notepad_vue/",
+  base: "/NotebookVue/",
   plugins: [
     vue(),
   ],
